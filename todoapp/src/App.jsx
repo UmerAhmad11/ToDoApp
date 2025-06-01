4bc8b44607609d3072f286a949b1e36343a84c47
UmerAhmad11 <sumerahmad2002@gmail.com>
2025-06-02 02:41:47 +0300

    ..

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import banner from './assets/banner.gif'
+import ToDo from './assets/ToDo.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header className = "App-header">
+      <img className = "logo"
+            src={ToDo}
+            alt="Header Logo"
+          />
+        <img className = "banner"
+            src={banner}
+            alt="Header Banner"
+          />
+      </header>
+
+      {/* Push content below fixed header */}
+      <main className="pt-[170px] px-8 text-center text-white">
+        <h1 className="text-xl md:text-3xl text-pink-400 tracking-widest mb-6">
+          Welcome to the <span className="text-cyan-400">ToDo App</span>
+        </h1>
+      </main>
+    </div>
+
   )
 }
 
