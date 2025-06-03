2240350255176c706d438d88f7ec04ea1d50557d
UmerAhmad11 <sumerahmad2002@gmail.com>
2025-06-03 15:51:05 +0300

    Added Sidebar

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,7 +1,33 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import banner from './assets/banner.gif'
 import ToDo from './assets/ToDo.svg'
+import { Container } from 'postcss'
+
+function Sidebar() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <div className="username">Umer Ahmad</div>
+      <div className="clock">{time.toLocaleTimeString()}</div>
+
+      <div className="calendar">
+        📅 {time.toLocaleDateString('en-GB', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        })}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,11 +45,18 @@ function App() {
           />
       </header>
 
+      <Sidebar />
+
       {/* Push content below fixed header */}
       <main className="pt-[170px] px-8 text-center text-white">
         <h1 className="text-xl md:text-3xl text-pink-400 tracking-widest mb-6">
           Welcome to the <span className="text-cyan-400">ToDo App</span>
         </h1>
+
+        <div className="container">
+          
+        </div>
+
       </main>
     </div>
 
